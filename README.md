bf1201f37cce16441104177dbfff41c4ccde9e45
erickHolguin <erihwong@espol.edu.ec>
2022-06-07 15:27:03 -0500

    Volviendo a <47fbc833099478f77a6e32a57bce6f231d630d9b>

---
+++ README.md
@@ -0,0 +1,2 @@
+# AMST2
+Repositorio creado para la resolución del lab1 de la clase de APLICACIONES MÓVILES Y SERVICIOS TELEMÁTICOS
--- app/src/main/java/com/example/amst2/MainActivity.java
+++ app/src/main/java/com/example/amst2/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.amst2;
+/*Mylene Cambios prueba*/
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
