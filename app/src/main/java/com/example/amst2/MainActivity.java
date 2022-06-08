3164698b894e05ef83120b3deba40b2f5c1cae2f
erickHolguin <erihwong@espol.edu.ec>
2022-06-07 19:45:19 -0500

    Avance del taller

--- app/src/main/java/com/example/amst2/MainActivity.java
+++ app/src/main/java/com/example/amst2/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.amst2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText txtNombre, txtApellido, txtUsuario, txtPasswd;
+    private Button btnLogin, btnRegistro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Referencias a los controles
+        txtUsuario = (EditText)findViewById(R.id.TextView_Usuario);
+        txtPasswd = (EditText)findViewById(R.id.TextView_Passwd);
+
+        btnLogin = (Button)findViewById(R.id.Btn_Login);
+        btnRegistro = (Button)findViewById(R.id.Btn_Registro);
+    }
+
+    public void registrarse(View view) {
+        Intent intent = new Intent(this, formulario_registro.class);
+        startActivity(intent);
+    }
+
+    public void login(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(), "Usted no cuenta con un usuario", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void onClick(View v) {
+        if(v.getId() == R.id.Btn_Login) {
+            Log.d("mensaje", "ingreso");
+        }else if(v.getId() == R.id.Btn_Registro){
+            //nada
+        }
     }
 }
