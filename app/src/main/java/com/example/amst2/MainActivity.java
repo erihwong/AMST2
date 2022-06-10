c7bae848287258bab51713a5ef04808084811532
unknown <angdesan99@gmail.com>
2022-06-09 21:34:44 -0500

    actualizacion de menu

--- app/src/main/java/com/example/amst2/MainActivity.java
+++ app/src/main/java/com/example/amst2/MainActivity.java
@@ -2,9 +2,9 @@ package com.example.amst2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log; import
-        android.view.View; import
-        android.widget.Button;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
@@ -26,8 +26,10 @@ public class MainActivity extends AppCompatActivity {
     public void login(View view) {
         Toast toast=Toast.makeText(getApplicationContext(),"Usted no cuenta con un usuario",Toast.LENGTH_SHORT);
         toast.show();
-        Intent intent = new Intent(getApplicationContext(),MenuLateralActivity.class);
+        Intent intent = new Intent(this,MenuActivity.class);
         startActivity(intent);
+
+
 
     }
     public void onClick(View v) {
